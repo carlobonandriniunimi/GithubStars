d8f708b80d6b054a433c4023661bd899b8659b22
carlobonandriniunimi <carlo.bonandrini@studenti.unimi.it>
2023-03-11 11:09:19 +0100

    Exploring repo fields

--- python_repos.py
+++ python_repos.py
@@ -18,6 +18,23 @@ print(f"Status code: {r.status_code}")
 
 # Convert the response object to a dictionary.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Process the results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository.
+# repo_dict = repo_dicts[0]
+# print(f"\nKeys: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
